a16c39b279d82fef23fefc869f4b7a902e118d21
Shehan <srajapak@mail.umw.edu>
2014-11-16 18:25:59 +0000

    completed final part 1

--- Final Exam 1/phy.c
+++ Final Exam 1/phy.c
@@ -10,33 +10,26 @@
 #include  <sys/ipc.h>
 #include  <sys/shm.h>
 
-const int MAX = 13;
+const int MAX = 15;
 #define SHMSZ     1024
-static void doFib(int n, int doPrint);
+static void phy(int n, int doPrint);
 
 /*
  * unix_error - unix-style error routine.
  */
 inline static void 
-unix_error(char *msg)
-{
+unix_error(char *msg){
 	fprintf(stdout, "%s: %s\n", msg, strerror(errno));
 	exit(1);
 }
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	int arg;
-	int print;
 
 	if(argc != 2){
 		fprintf(stderr, "Usage: fib <num>\n");
 		exit(-1);
-	}
-
-	if(argc >= 3){
-		print = 1;
 	}
 
 	arg = atoi(argv[1]);
@@ -44,7 +37,7 @@ int main(int argc, char **argv)
 		fprintf(stderr, "number must be between 0 and %d\n", MAX);
 		exit(-1);
 	}
-	int *result[13];
+	int *result[15];
 	int shmid;
 	key_t key;
 	key = 1234;
@@ -52,15 +45,15 @@ int main(int argc, char **argv)
 		unix_error("ERROR SHMGET");
 	}
 	int x;
-	for(x = 0; x < 13; x++){  
+	for(x = 0; x < 15; x++){  
 		if ((result[x] = (int *)shmat(shmid, NULL, 0)) == (int *) -1){
 			unix_error("ERROR SHMAT");
 		}
 	}
-	for(x = 0; x < 13; x++){
+	for(x = 0; x < 15; x++){
 		*result[x] = 0;
 	}
-	doFib(arg, 1);
+	phy(arg, 1);
 
 	return 0;
 }
@@ -73,10 +66,11 @@ int main(int argc, char **argv)
  * a new child for each call. Each process should call
  * doFib() exactly once.
  */
-static void doFib(int n, int doPrint){
+static void phy(int n, int doPrint){
 	int shmid;
  	key_t key;
- 	pid_t pid;
+ 	pid_t pid1;
+ 	pid_t pid2;
 	int status;
 	int *result;
 	key = 1234;
@@ -84,26 +78,39 @@ static void doFib(int n, int doPrint){
 		unix_error("ERROR SHMGET");
 	}
 	int x;
-	for(x = 0; x < 13; x++){  
+	for(x = 0; x < 15; x++){  
 		if ((result = (int *)shmat(shmid, NULL, 0)) == (int *) -1){
 			unix_error("ERROR SHMAT");
 		}
 	}
-	if(n < 2){
-		if(doPrint){
-			printf("%d\n", n);
+	if(n < 3){
+		int phylNum;
+		if(n < 2){
+			phylNum = n;
 		}
 		else{
-  	     		result[n] = n;
+			phylNum = 1;
+		}
+		if(doPrint){
+			printf("%d\n", phylNum);
+		}
+		else{
+  	     		result[n] = phylNum;
 		}
 		exit(0);
 	}
-	pid = fork();
-	if(pid == 0){
-		doFib(n-1, 0);
+	pid1 = fork();
+	if(pid1 == 0){
+		phy(n-1, 0);
 	}
-	waitpid(pid, &status, 0); //parent process waits til this finishes
-	result[n] = (result[n-1]) + (result[n-2]);
+	pid2 = fork();
+	if(pid2 == 0){
+		phy(n-2, 0);
+	}
+
+	waitpid(pid1, &status, 0); //parent process waits til this finishes
+	waitpid(pid2, &status, 0); //parent process waits til this finishes
+	result[n] = (result[n-1]) + (result[n-2] + result[n-3]);
 	if(doPrint){
 		printf("%d\n", result[n]); //prints final answer if doPrint is 1
 	}
